0bce8668a56d562767e68a29cc015a3b7b672d65
BuildTools <unconfigured@null.spigotmc.org>
2016-04-15 17:48:08 +0900

    Initial commit

---
+++ src/main/java/com/ittekikun/plugin/eewalert/EEWAlert.java
@@ -0,0 +1,18 @@
+package com.ittekikun.plugin.eewalert;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EEWAlert  extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        // TODO ここに、プラグインが有効化された時の処理を実装してください。
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // TODO ここに、プラグインが無効化された時の処理を実装してください。
+    }
+}
